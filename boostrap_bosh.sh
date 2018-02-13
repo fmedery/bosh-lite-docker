1aa5e2b4b76e4635da6e6011f73df61fff00658d
Frederic Medery <fmedery@gmail.com>
2018-02-13 16:01:13 -0500

    update scripts

--- boostrap_bosh.sh
+++ boostrap_bosh.sh
@@ -2,11 +2,17 @@
 set -e
 # set -x
  
-source ./modules
+source ./functions
 
 # get bosh-deployment as a submodule
 git submodule init
 git submodule update
+
+# bosh environment
+export BOSH_ENVIRONMENT="docker"
+
+# default bosh admin user
+export BOSH_CLIENT="admin"
 
 message_info "check if bosh director is up if not run create.sh to build it"
 
@@ -16,13 +22,9 @@ then
   ./create.sh
 else
   message_info "bosh director is up"
+  message_completed
+  exit 0
 fi
-
-# bosh environment
-export BOSH_ENVIRONMENT="docker"
-
-# default bosh admin user
-export BOSH_CLIENT="admin"
 
 # this command will extract the admin password
 export BOSH_CLIENT_SECRET=$(bosh int ./creds.yml --path /admin_password)
@@ -42,13 +44,13 @@ sed -i -e 's=\[8.8.8.8\]=((dns))=g' ./cloud-config.yml
 
 # apply cloud-config for docker
 message_info "apply cloud-config with opsfiles-cloud-config.yml opsfile to add Desjardins DNS"
-bosh --non-interactive --environment=$BOSH_ENVIRONMENT --client=${BOSH_CLIENT} --client-secret=${BOSH_CLIENT_SECRET} update-cloud-config cloud-config.yml -o ./opsfiles-cloud-config.yml
+bosh --non-interactive --environment=${BOSH_ENVIRONMENT} --client=${BOSH_CLIENT} --client-secret=${BOSH_CLIENT_SECRET} update-cloud-config cloud-config.yml -o ./opsfiles-cloud-config.yml
 
 # download the latest stemcell and upload it to the director
 # we will use /tmp as the target folder
 message_info "get latest stemcell and upload it"
 wget --content-disposition -q -N -P /tmp/ -N https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent 
-bosh --non-interactive --environment=$BOSH_ENVIRONMENT --client=${BOSH_CLIENT} --client-secret=${BOSH_CLIENT_SECRET} upload-stemcell /tmp/bosh-stemcell-*-warden-boshlite-ubuntu-trusty-go_agent.tgz
+bosh --non-interactive --environment=${BOSH_ENVIRONMENT} --client=${BOSH_CLIENT} --client-secret=${BOSH_CLIENT_SECRET} upload-stemcell /tmp/bosh-stemcell-*-warden-boshlite-ubuntu-trusty-go_agent.tgz
 
 # if docker socket is not 777 we need change it
 if ! ls -l /var/run/docker.sock|grep srwxrwxrwx -q
@@ -57,12 +59,6 @@ then
   sudo chmod 777 /var/run/docker.sock
 fi
 
-message_info "setup completed"
-
-echo '***********************'
-echo "environment   : ${BOSH_ENVIRONMENT} "
-echo "client        : ${BOSH_CLIENT}"
-echo "client-secret : ${BOSH_CLIENT_SECRET}"
-echo '***********************'
+message_completed
 
 # vim: set ft=bash:
